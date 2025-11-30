fe40500bffd33b86a9b616163eb38cb757619d70
gavrylyuk <ggavrylyuk@gmail.com>
2025-11-30 22:49:41 +0100

    feat: add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  const port = process.env.PORT ? Number(process.env.PORT) : 4000;
+  await app.listen(port);
 }
 bootstrap();
